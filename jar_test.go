d1276d5d612035d6e858d937616442d863d61dc0
glen newton <glen.newton@gmail.com>
2015-03-19 06:06:13 -0400

    Added no jar file test

--- jar_test.go
+++ jar_test.go
@@ -2,13 +2,16 @@ package jargo
 
 import (
 	//"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"testing"
 )
 
 const jar1BaseUrl = "http://search.maven.org/remotecontent?filepath=org/apache/lucene/lucene-analyzers-common/5.0.0/"
-const jar1Name = "lucene-analyzers-common-5.0.0.jar"
+const jar1Name = "lucene-analyzers-stempel-5.0.0.jar"
+
+const badJarName = "zzz-foobar------_______"
 
 //resp, err := http.Get("http://example.com/")"
 
@@ -18,18 +21,22 @@ func TestMain(m *testing.M) {
 }
 
 func TestValidJarFile_JarInfo(t *testing.T) {
-	_, err := GetJarInfo("lucene-analyzers-stempel-5.0.0.jar")
-	//err, jar := MakeJar("lucene-1.4.3.jar")
-	//err, jar := MakeJar("/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.31-3.b13.fc21.x86_64/lib/tools.jar")
-	//err, _ := MakeJar("/usr/lib/jvm-exports/java-1.8.0-openjdk-1.8.0.31-3.b13.fc21.x86_64/jaas-1.8.0.31.jar")
+	_, err := GetJarInfo(jar1Name)
 	if err != nil {
 		t.FailNow()
 	}
-	//fmt.Println(jar)
+}
+
+func TestMissingJarFile_JarInfo(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+	_, err := GetJarInfo(badJarName)
+	if err == nil {
+		t.FailNow()
+	}
 }
 
 func TestValidJarFile_JarManifest(t *testing.T) {
-	manifest, err := GetManifest("lucene-analyzers-stempel-5.0.0.jar")
+	manifest, err := GetManifest(jar1Name)
 	//err, jar := MakeJar("lucene-1.4.3.jar")
 	//err, jar := MakeJar("/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.31-3.b13.fc21.x86_64/lib/tools.jar")
 	//err, _ := MakeJar("/usr/lib/jvm-exports/java-1.8.0-openjdk-1.8.0.31-3.b13.fc21.x86_64/jaas-1.8.0.31.jar")
